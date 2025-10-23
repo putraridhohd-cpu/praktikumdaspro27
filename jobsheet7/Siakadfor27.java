0aec2d48cf8fa1d2ebfc23b4c2509f6323bb3f46
praktikumdaspro27 <email@putraridhohd.com>
2025-10-23 11:45:24 +0700

    modifikasi percobaan 1

--- jobsheet7/Siakadfor27.java
+++ jobsheet7/Siakadfor27.java
@@ -5,6 +5,7 @@ public class Siakadfor27 {
         Scanner input = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100 ;
+        int lulus = 0, tidakLulus = 0;
         for (int  i= 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = input.nextDouble();
@@ -15,9 +16,17 @@ public class Siakadfor27 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            // cek kelulusan untuk setiap mahasiswa
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
         System.out.println("\nNilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
         input.close();
     }
-}
+    }
