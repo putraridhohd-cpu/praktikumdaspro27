1faa28adad61476e4118e567130898e0238a42f4
praktikumdaspro27 <email@putraridhohd.com>
2025-10-23 13:11:43 +0700

    modifikasi percobaan 1 (jobsheet 5)

--- jobsheet5/ifcetakKRS27.java
+++ jobsheet5/ifcetakKRS27.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class ifcetakKRS27 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+
         System.out.println("=== Cetak KRS ===");
-        System.out.println("apakah UKT udah lunas? (true/false): ");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = input.nextBoolean();
+
         if (uktLunas) {
-            System.out.println("pembayaran UKT terverifikasi");
+            System.out.println("Pembayaran UKT terverifikasi.");
         } else {
-            System.out.println("silahkan cetak KRS dan minta tanda tangan DPA");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA.");
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.");
         }
+
+        input.close();
     }
-        
-        }
+}
