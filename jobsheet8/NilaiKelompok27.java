4860bb292f1b12827c681f91fe93940cd3c6e17c
valentridho <putraridhohd@gmail.com>
2025-11-03 18:28:15 +0700

    modifikasi percobaan ke-4 (jobsheet 8)

--- jobsheet8/NilaiKelompok27.java
+++ jobsheet8/NilaiKelompok27.java
@@ -5,19 +5,36 @@ public class NilaiKelompok27 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i = 1;
-while (i <= 6) {
-    System.out.println("kelompok" + i);
-    int totalnilai = 0;
-    for (int j = 1; j <= 5; j++) {
-        System.out.println("nilai dari kelompok penilai" + j + ": ");
-        int nilai = sc.nextInt();
-        totalnilai += nilai;
+
+        double rataTertinggi = 0;
+        String kelompokTertinggi = "";
+
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+            int totalnilai = 0;
+
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("Nilai dari kelompok penilai " + j + ": ");
+                int nilai = sc.nextInt();
+                totalnilai += nilai;
+            }
+
+            double ratanilai = totalnilai / 5.0;
+            System.out.printf("Rata-rata nilai kelompok %d: %.2f%n%n", i, ratanilai);
+
+            if (ratanilai > rataTertinggi) {
+                rataTertinggi = ratanilai;
+                kelompokTertinggi = "Kelompok " + i;
+            } else if (ratanilai == rataTertinggi) {
+                kelompokTertinggi += " dan Kelompok " + i;
+            }
+
+            i++;
+        }
+        System.out.println("=================================");
+        System.out.printf("Kelompok dengan rata-rata tertinggi adalah %s dengan nilai rata-rata %.2f%n",
+                          kelompokTertinggi, rataTertinggi);
+
+        sc.close();
     }
-    int ratanilai = totalnilai / 5;
-    System.out.println("Rata-rata nilai kelompok " + i + ": " + ratanilai);
-    i++;
-    
-}
-sc.close();
-}
 }
