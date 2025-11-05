94fe3055db2d93871cad8c6575db6ddeea5cfafd
valentridho <putraridhohd@gmail.com>
2025-11-05 15:34:36 +0700

    Percobaan 3 (Jobsheett 9)

---
+++ jobsheet9/ArrayRataNilai27.java
@@ -0,0 +1,23 @@
+package jobsheet9;
+import java.util.Scanner;
+
+public class ArrayRataNilai27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nilaiMhs= new int[10];
+        double ttottal = 0;
+        double rata2;
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
+            nilaiMhs[i] = sc.nextInt();
+        }
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            ttottal += nilaiMhs[i];
+        }
+        rata2 = ttottal / nilaiMhs.length;
+        System.out.println("Rata-rata nilai = " + rata2);
+        sc.close();
+    }
+}
+
+        
