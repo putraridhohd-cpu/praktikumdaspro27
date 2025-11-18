bddd23b2afa7a6f164e78628a93bf47261c76ad1
valentridho <putraridhohd@gmail.com>
2025-11-18 20:00:49 +0700

    modifikasi percobaan 1 (jobsheet 10)

--- jobsheet10/Bioskop27.java
+++ jobsheet10/Bioskop27.java
@@ -11,10 +11,27 @@ public class Bioskop27 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
+
 
         System.out.printf("%s\t%s\n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s\t%s\n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s\t%s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s\t%s\n", penonton[3][0], penonton[3][1]);
+
+        System.out.println(penonton.length);
+        for (int i = 0; i < penonton.length; i++) {
+        System.out.println("penonton pada baris ke-" + (i + 1) + ": " + String.join(", ", penonton[i]));
+        }
+        System.out.println("penonton pada baris ke-3: ");
+        for (String i: penonton[2]){
+            System.out.println(i);
+        }
+        }
     }
-}
+        
+            
+
+        
+    
+
