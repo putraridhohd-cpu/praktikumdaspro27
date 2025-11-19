170c48ad0fe759ea4163b1ef81cb8a5663354c36
valentridho <putraridhohd@gmail.com>
2025-11-19 21:28:10 +0700

    Modifikasi Percobaan 3 (Jobsheet 10)

--- jobsheet10/numbers27.java
+++ jobsheet10/numbers27.java
@@ -10,6 +10,13 @@ public class numbers27 {
         mynumbers[1] = new int[3];
         mynumbers[2] = new int[1];
 
+        for (int i = 0; i < mynumbers.length; i++) {
+            System.out.println(Arrays.toString(mynumbers[i]));
+        }
+        for (int i = 0; i < mynumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + mynumbers[i].length);
+        }
+
     }
     
 }
