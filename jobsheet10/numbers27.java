bf14e7480cbd4bf8e0c90001663ba646a8f9400b
valentridho <putraridhohd@gmail.com>
2025-11-19 20:35:30 +0700

    Percobaan 3 (Jobsheet 10)

---
+++ jobsheet10/numbers27.java
@@ -0,0 +1,15 @@
+package jobsheet10;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class numbers27 {
+    public static void main(String[] args) {
+        int[][] mynumbers = new int [3][];
+        mynumbers[0] = new int[5];
+        mynumbers[1] = new int[3];
+        mynumbers[2] = new int[1];
+
+    }
+    
+}
