b187508f564b59a203b1f6d15adc4a2b65eff474
valentridho <putraridhohd@gmail.com>
2025-11-20 09:43:07 +0700

    Modifikasi Percobaan 4  (Jobsheet 10)

--- jobsheet10/SIAKAD27.java
+++ jobsheet10/SIAKAD27.java
@@ -4,29 +4,34 @@ import java.util.Scanner;
 public class SIAKAD27 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("masukan jumlah mahasiwa");
+        int jumlahmahasiswa = sc.nextInt();
+        System.out.println("masukan jumlah mata kuliah?");
+        int jumlahmatkul = sc.nextInt();
 
-        int [][] nilai = new int[4][3];
+
+        int [][] nilai = new int[jumlahmahasiswa][jumlahmatkul];
        
 
-        for (int i = 0; i < nilai.length; i++) {
+        for (int i = 0; i < jumlahmahasiswa; i++) {
             System.out.println("input nilai mahasiswa ke-" + (i + 1));
             double totalpersiswa = 0;
             
-            for (int j = 0; j < nilai[i].length; j++) {
+            for (int j = 0; j < jumlahmatkul; j++) {
                 System.out.print("nilai mata kuliah " + (j + 1) + ": ");
                 nilai[i][j] = sc.nextInt();
                 totalpersiswa += nilai[i][j];
             }
             
-            System.out.println("nilai rata rata: " + totalpersiswa/3);
+            System.out.println("Rata-rata mahasiswa ke-" + (i + 1) + ": " + (totalpersiswa / jumlahmatkul));
     }
          System.out.println("\n=====================================");
          System.out.println("Rata rata nilai setiap mata kuliah: ");
 
-         for (int j = 0; j < 3; j++) {
+         for (int j = 0; j < jumlahmatkul; j++) {
             double totalpermatkul = 0;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < jumlahmahasiswa; i++) {
                 totalpermatkul += nilai [i][j];
             
             }
