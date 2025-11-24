9e77247fcf50a2f8fa210cc118547f628b55d507
valentridho <putraridhohd@gmail.com>
2025-11-24 09:11:40 +0700

    ini cuman tes saja

---
+++ jobsheet11/tes_saja.java
@@ -0,0 +1,26 @@
+package jobsheet11;
+
+public class tes_saja {
+    static double nilai(int a, int b, int c, int d) {
+        double rata = (a+b+c+d)/4.0;
+        return rata;
+
+    }
+     public static void main(String[] args) {
+        double hasil = nilai(1,2,3,4);
+        System.out.println(hasil);
+        
+        
+
+
+     
+
+
+            
+            }
+        }
+            
+
+    
+    
+
