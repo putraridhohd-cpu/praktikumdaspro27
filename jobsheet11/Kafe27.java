f64131376e14c61bfc3d43741dbee2208a5541ce
valentridho <putraridhohd@gmail.com>
2025-12-03 18:43:16 +0700

    modifikasi percobaan 2 (Jobsheet 11)

--- jobsheet11/Kafe27.java
+++ jobsheet11/Kafe27.java
@@ -1,9 +1,20 @@
 package jobsheet11;
+
 public class Kafe27 {
-    public static void Menu(String namaPelanggan, boolean isMember){
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo){
         System.out.println("Selamat datang, " + namaPelanggan);
+        
+
         if(isMember){
-            System.out.println("anda adalah member,dapat diskon 10% untuk setiap pembelian!");
+            System.out.println("Anda adalah member, dapat diskon 10% untuk setiap pembelian!");
+        }
+
+        if(kodePromo.equals("DISKON50")){
+            System.out.println("Kode promo valid: Anda mendapatkan diskon 50%!");
+        } else if(kodePromo.equals("DISKON30")){
+            System.out.println("Kode promo valid: Anda mendapatkan diskon 30%!");
+        } else {
+            System.out.println("Kode promo invalid.");
         }
 
         System.out.println("=====MENU RESTO KAFE =====");
@@ -17,8 +28,7 @@ public class Kafe27 {
         System.out.println("Silahkan pilih menu yang anda inginkan");
     }
 
-        public static void main(String[] args) {
-            Menu("Valent", true);
-            
+    public static void main(String[] args) {
+        Menu("budi", true, "DISKON50");
     }
-    }
+}
