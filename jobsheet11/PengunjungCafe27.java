b2a122828c8de4f072929d3fe1117115d714d7a9
valentridho <putraridhohd@gmail.com>
2025-12-04 13:19:34 +0700

    Modifikasi percobaan 4 (Jobsheet 11)

--- jobsheet11/PengunjungCafe27.java
+++ jobsheet11/PengunjungCafe27.java
@@ -1,10 +1,11 @@
 package jobsheet11;
+
 public class PengunjungCafe27 {
 
     static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar nama pengunjung:");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
         }
     }
 
