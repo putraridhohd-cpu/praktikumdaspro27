1a54ce3e1e24fcfaa552c0983b40bd487c362fdd
valentridho <putraridhohd@gmail.com>
2025-12-21 23:01:14 +0700

    modifikasi percobaan 2 (jobsheet 12)

--- Jobsheet12/Percobaan2.java
+++ Jobsheet12/Percobaan2.java
@@ -2,20 +2,29 @@ package Jobsheet12;
 import java.util.Scanner;
 
 public class Percobaan2 {
+
     static int hitungpangkat(int x, int y) {
         if (y == 0) {
+            System.out.print("1");
             return 1;
         } else {
+            System.out.print(x + "x");
             return x * hitungpangkat(x, y - 1);
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Bilangan yang dihitung: ");
         int bilangan = sc.nextInt();
+
         System.out.print("Pangkat: ");
         int pangkat = sc.nextInt();
-        System.out.println(hitungpangkat(bilangan, pangkat));
-        
+
+        int hasil = hitungpangkat(bilangan, pangkat);
+        System.out.println(" = " + hasil);
+
+        sc.close();
     }
 }
